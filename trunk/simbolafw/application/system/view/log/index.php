2ce300ce040f216c3b846be344aaa29acd9592e1
farajfarook <farajfarook@b5cebaac-146a-a145-46be-9f3be0cafb34>
2014-02-28 12:20:40 +0000

    changed the logger column names in the display

--- trunk/simbolafw/application/system/view/log/index.php
+++ trunk/simbolafw/application/system/view/log/index.php
@@ -5,14 +5,15 @@ $grid->title = "Log";
 $grid->usepager = true;
 
 $grid->setDirect(true);
-$grid->setDataSource('system', 'logger', 'log', '_date');
-$grid->addColModel("Date", "_date", "130", true, 'left');
-$grid->addColModel("Type", "_type", "100", true, 'left');
-$grid->addColModel("Message", "_message", "300", true, 'left');
+$grid->setDataSource('system', 'logger', 'log', 'date');
+$grid->addColModel("Date", "date", "130", true, 'left');
+$grid->addColModel("Type", "type", "40", true, 'left');
+$grid->addColModel("Trace", "trace", "100", false, 'left');
+$grid->addColModel("Message", "message", "1000", true, 'left');
 
-$grid->addSearchItem("Date", '_date', false);
-$grid->addSearchItem("Type", '_type', true);
-$grid->addSearchItem("Message", '_message', false);
+$grid->addSearchItem("Date", 'date', false);
+$grid->addSearchItem("Type", 'type', true);
+$grid->addSearchItem("Message", 'message', false);
 
 echo $grid->getDisplayData();
 ?>
