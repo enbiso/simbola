781fb90fa20a6dbd7fbdf1bef762c29d3a5ae653
farajfarook <farajfarook@b5cebaac-146a-a145-46be-9f3be0cafb34>
2014-06-04 18:42:19 +0000

    cron jobs

--- trunk/simbolafw/simbola/core/component/transaction/Transaction.php
+++ trunk/simbolafw/simbola/core/component/transaction/Transaction.php
@@ -1,38 +1,85 @@
 <?php
+
 namespace simbola\core\component\transaction;
+
 /**
  * Description of Cron
  *
  * @author farflk
  */
-class Transaction extends \simbola\core\component\system\lib\Component{       
-    
+class Transaction extends \simbola\core\component\system\lib\Component {
+
+    private $cron;
+
     /**
      * Function used to call on Cron jobs
      * @param type $cronId Cron Identification
      */
     public function cron($cronId) {
-        if(!$this->updateCronTable($cronId)){
-            slog_syserror(__METHOD__, "Cron table update failed for Cron ID {$cronId}");   
+        try {            
+            if (!$this->updateGetCronObject($cronId)) {
+                slog_syserror(__METHOD__, "Cron table update failed for Cron ID {$cronId}");
+                return false;
+            }
+            $this->initCron();
+            //$this->getNextJob();
+            $this->finishCron();
+        } catch (\Exception $ex) {
+            slog_syserror(__METHOD__, "Cron Error: " . $ex->getMessage());
+            $this->finishCron();
             return false;
         }
     }
-    
+
+    /**
+     * Finishing the cron job
+     */
+    private function finishCron() {
+        $this->cron->job_count--;
+        if ($this->cron->job_count <= 0) {
+            $this->cron->job_count = 0;
+        }
+        $this->cron->save();
+        if ($this->cron->job_count == 0) {
+            $this->cron->state("ready");
+        }
+    }
+
+    /**
+     * Initializing the cron job
+     */
+    private function initCron() {
+        switch ($this->cron->state()) {
+            case 'execute':
+                $this->cron->job_count++;
+                $this->cron->save();
+                break;
+            case 'ready':
+                $this->cron->job_count = 1;
+                $this->cron->save();
+                $this->cron->state("execute");
+                break;
+        }
+    }
+
     /**
      * Update cron table
      * @param type $cronId Cron Identification id
-     * @return type
+     * @return boolean
      */
-    private function updateCronTable($cronId) {
-        $cron = \application\system\model\transaction\Cron::find('first', array('id' => $cronId));
-        if($cron == NULL){
-            $cron = new \application\system\model\transaction\Cron();
-            $cron->id = $cronId;
-            $cron->executed_count = 0;
+    private function updateGetCronObject($cronId) {
+        $this->cron = \application\system\model\transaction\Cron::find('first', array('id' => $cronId));
+        if ($this->cron == NULL) {
+            $this->cron = new \application\system\model\transaction\Cron(array(
+                'id' => $cronId,
+                'count' => 0,
+            ));
+        } else {
+            $this->cron->interval = time() - date_timestamp_get($this->cron->executed);
         }
-        $cron->executed_count++;
-        $cron->last_executed = date("Y-m-d H:i:s");
-        return $cron->save();
+        $this->cron->count++;
+        $this->cron->executed = date("Y-m-d H:i:s");
+        return $this->cron->save();
     }
-    
+
 }
---
+++ trunk/simbolafw/simbola/core/component/transaction/lib/PHPScriptJob.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace simbola\core\component\transaction\lib;
+/**
+ * Description of PHPScriptJob
+ *
+ * @author Faraj
+ */
+class PHPScriptJob extends AbstractJob{
+    //put your code here
+}
